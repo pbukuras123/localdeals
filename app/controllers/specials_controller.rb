d3b8b8daae47e69b4546dcbca9db27992e3ceca0
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2021-08-03 19:24:13 -0400

    Complete Create Action for Specials

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -8,4 +8,18 @@ def index
 	@specials = Special.all
 end
 
+def new
+	@special = Special.new
+end
+
+def create
+	@special = Special.new(params.require(:special).permit(:day, :title, :description))
+	if @special.save
+		flash[:notice] = "Special was created successfully"
+ 		redirect_to (@special)
+	else render 'new'
+
+	end
+end
+
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,4 +1,4 @@
 class Special < ApplicationRecord
-	validates :day, presence: true, length: { minimum: 6, maximum: 10}
-	validates :title, presence: true, length: { minimum: 6, maximum: 100}
+	validates :day, presence: true, length: { minimum: 5, maximum: 10}
+	validates :title, presence: true, length: { minimum: 3, maximum: 100}
 end
