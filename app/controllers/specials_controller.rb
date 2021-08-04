8170f1a09034f07ab89ecbafefbf36a00913b649
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2021-08-04 18:30:32 -0400

    Finish edit and updated action

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -12,6 +12,10 @@ def new
 	@special = Special.new
 end
 
+def edit
+	@special = Special.find(params[:id])
+end
+
 def create
 	@special = Special.new(params.require(:special).permit(:day, :title, :description))
 	if @special.save
@@ -19,6 +23,16 @@ def create
  		redirect_to (@special)
 	else render 'new'
 
+	end
+end
+
+def update
+	@special = Special.find(params[:id])
+	if @special.update(params.require(:special).permit(:day, :title, :description))
+		flash[:notice] = "Special was updated successfully"
+		redirect_to @special
+	else
+		render 'edit'
 	end
 end
 
